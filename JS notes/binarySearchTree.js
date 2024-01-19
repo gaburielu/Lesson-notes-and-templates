8d2abe43f966b7ba276ad6d2492c190d46dce98d
GabrielX <97820374+gaburielu@users.noreply.github.com>
2024-01-20 00:48:22 +0800

    adding depthFirstTreversal and find node function

--- JS notes/binarySearchTree.js
+++ JS notes/binarySearchTree.js
@@ -5,7 +5,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
@@ -38,30 +38,80 @@ function merge(left, right) {
 }
 
 function removeDuplicate(arr) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!newArray.includes(arr[i])) {
-      newArray.push(arr[i]);
-    }
-  }
-  return newArray;
+  return [...new Set(arr)];
 }
 
-class node {
-  constructor(value, right = null, left = null) {
+function createBST(arr, start, end) {
+  if (arr.length === 0 || arr === null || start > end) {
+    return null;
+  }
+  const mid = Math.floor((start + end) / 2);
+  const root = new Node(arr[mid]);
+  root.left = createBST(arr, start, mid - 1);
+  root.right = createBST(arr, mid + 1, end);
+  return root;
+}
+
+class Node {
+  constructor(value) {
     this.value = value;
-    this.right = right;
-    this.left = left;
+    this.left = null;
+    this.right = null;
   }
 }
 
-class binarySearchTree {
+function depthFirstPreorderTraversal(root) {
+  if (!root) return;
+  console.log(root.value);
+  depthFirstPreorderTraversal(root.left);
+  depthFirstPreorderTraversal(root.right);
+}
+
+function find(root, value) {
+  if (!root) {
+    return null;
+  }
+  if (root.value === value) {
+    return root;
+  } else if (root.value > value) {
+    return find(root.left, value);
+  } else if (root.value < value) {
+    return find(root.right, value);
+  } else {
+    return null;
+  }
+}
+
+function treeDepth(root) {
+  if (root == null) {
+    return 0;
+  } else {
+    let heightLeft = treeDepth(root.left);
+    let heightRight = treeDepth(root.right);
+    return Math.max(heightLeft, heightRight) + 1;
+  }
+}
+
+function nodeDepth(root, value) {
+  let node = find(root, value);
+  return treeDepth(node);
+}
+
+class BinarySearchTree {
   constructor(array) {
-    this.root = null;
+    this.root = this.createTree(array);
+  }
+
+  createTree(arr) {
+    let sanitizedArray = mergeSort(removeDuplicate(arr));
+    return createBST(sanitizedArray, 0, sanitizedArray.length - 1);
   }
 }
 
-let foo = [1, 23, 7, 4, 23, 8, 9, 43, 43, 10, 3, 5, 7, 99, 9, 67, 23, 54];
-let bar = mergeSort(foo);
-console.log(bar);
-console.log(removeDuplicate(bar));
+let foo = [
+  21, 23, 71, 4, 23, 48, 89, 43, 43, 90, 30, 55, 7, 99, 19, 67, 23, 54,
+];
+console.log(mergeSort(removeDuplicate(foo)));
+let tree = new BinarySearchTree(foo);
+console.log(prettyPrint(tree.root));
+console.log(nodeDepth(tree.root, 21));
