4e4886f0daf0c250e59c3ac78f1ebe416ad585bf
GabrielX <97820374+gaburielu@users.noreply.github.com>
2024-01-21 19:53:07 +0800

    Finishing knightsTravails assignment from the ODIN

--- JS notes/binarySearchTree.js
+++ JS notes/binarySearchTree.js
@@ -60,26 +60,11 @@ class Node {
   }
 }
 
-function depthFirstPreorderTraversal(root) {
-  if (!root) return;
-  console.log(root.value);
-  depthFirstPreorderTraversal(root.left);
-  depthFirstPreorderTraversal(root.right);
-}
-
-function find(root, value) {
-  if (!root) {
-    return null;
-  }
-  if (root.value === value) {
-    return root;
-  } else if (root.value > value) {
-    return find(root.left, value);
-  } else if (root.value < value) {
-    return find(root.right, value);
-  } else {
-    return null;
-  }
+function depthFirstPreorderTraversal(node) {
+  if (!node) return;
+  console.log(node.value);
+  depthFirstPreorderTraversal(node.left);
+  depthFirstPreorderTraversal(node.right);
 }
 
 function treeDepth(root) {
@@ -92,7 +77,7 @@ function treeDepth(root) {
   }
 }
 
-function nodeDepth(root, value) {
+function nodeHeight(root, value) {
   let node = find(root, value);
   return treeDepth(node);
 }
@@ -106,12 +91,230 @@ class BinarySearchTree {
     let sanitizedArray = mergeSort(removeDuplicate(arr));
     return createBST(sanitizedArray, 0, sanitizedArray.length - 1);
   }
+
+  insert(value) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(value);
+      return;
+    } else {
+      const searchTree = function (node) {
+        if (value < node.value) {
+          if (node.left === null) {
+            node.left = new Node(value);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (value > node.value) {
+          if (node.right === null) {
+            node.right = new Node(value);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      return searchTree(node);
+    }
+  }
+
+  delete(value) {
+    const removeNode = function (node, value) {
+      if (node === null) {
+        return null;
+      }
+      if (value == node.value) {
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        if (node.left == null) {
+          return node.right;
+        }
+        if (node.right == null) {
+          return node.left;
+        }
+
+        let tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.value = tempNode.value;
+        node.right = removeNode(node.right, tempNode.value);
+        return node;
+      } else if (value < node.value) {
+        node.left = removeNode(node.left, value);
+        return node;
+      } else {
+        node.right = removeNode(node.right, value);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, value);
+  }
+
+  sumOfNodes(node) {
+    if (node === null) {
+      return 0;
+    }
+    let sum = 0;
+    const queue = [node];
+    while (queue.length > 0) {
+      const current = queue.shift();
+      sum++;
+      if (current.left !== null) {
+        queue.push(current.left);
+      }
+      if (current.right !== null) {
+        queue.push(current.right);
+      }
+    }
+    return sum;
+  }
+
+  _find(node, value) {
+    if (!node) {
+      return null;
+    }
+    if (node.value === value) {
+      return node;
+    } else if (node.value > value) {
+      return this._find(node.left, value);
+    } else if (node.value < value) {
+      return this._find(node.right, value);
+    } else {
+      return null;
+    }
+  }
+
+  find(value) {
+    return this._find(this.root, value);
+  }
+
+  height(node) {
+    return treeDepth(node);
+  }
+
+  depth(node) {
+    if (this.root === null || !node) {
+      return -1;
+    }
+    let current = this.root;
+    let currentDepth = 0;
+    while (current !== null) {
+      if (current.value === node.value) {
+        return currentDepth;
+      } else if (current.value > node.value) {
+        current = current.left;
+      } else if (current.value < node.value) {
+        current = current.right;
+      } else {
+        return -1;
+      }
+      currentDepth++;
+    }
+    return -1;
+  }
+
+  levelOrder(node, callback = null){
+    if (node === null) {
+      return 0;
+    }
+    const result = [];
+    const queue = [node];
+    while (queue.length > 0) {
+      const current = queue.shift();
+      if(callback){
+        callback(current);
+      } else {
+        result.push(current.value);
+      }
+      if (current.left !== null) {
+        queue.push(current.left);
+      }
+      if (current.right !== null) {
+        queue.push(current.right);
+      }
+    }
+    return result;
+  }
+
+  postOrderTraversal(node, callback = null) {
+    const result = [];
+    function traverse(currentNode) {
+      if (currentNode) {
+        traverse(currentNode.left);
+        traverse(currentNode.right);
+        if (callback) {
+          callback(currentNode);
+        } else {
+          result.push(currentNode.value);
+        }
+      }
+    }
+    traverse(node);
+    return result;
+  }
+
+  isBalanced(){
+    let sumLeft = this.sumOfNodes(this.root.left);
+    let sumRight = this.sumOfNodes(this.root.right);
+    if(sumLeft - sumRight > 1 || sumRight - sumLeft > 1){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  rebalance(node){
+    let newTreeArray = this.levelOrder(tree.root);
+    this.root = this.createTree(newTreeArray);
+  }
+
+  log(node) {
+    console.log(node.value);
+  }
 }
 
-let foo = [
-  21, 23, 71, 4, 23, 48, 89, 43, 43, 90, 30, 55, 7, 99, 19, 67, 23, 54,
-];
-console.log(mergeSort(removeDuplicate(foo)));
-let tree = new BinarySearchTree(foo);
+// let foo = [
+//   21, 23, 71, 4, 23, 48, 89, 43, 43, 90, 30, 55, 7, 99, 19, 67, 23, 54,
+// ];
+// let tree = new BinarySearchTree(foo);
+// tree.insert(20);
+// tree.insert(10);
+// console.log(prettyPrint(tree.root));
+// console.log(tree.isBalanced());
+// tree.rebalance();
+// console.log(prettyPrint(tree.root));
+// console.log(tree.isBalanced());
+// // tree.levelOrder(tree.root, tree.log);
+
+///TESTING///
+
+function randomNumberArray(){
+  let arr = [];
+  for(let i = 0; i < 16; i++){
+    arr.push(Math.floor(Math.random() * 100));
+  }
+  return arr;
+}
+
+let tree = new BinarySearchTree(randomNumberArray());
+
 console.log(prettyPrint(tree.root));
-console.log(nodeDepth(tree.root, 21));
+console.log(tree.isBalanced());
+
+let newArr = randomNumberArray();
+for(let i = 0; i < newArr.length;i++){
+  tree.insert(newArr[i]);
+}
+
+console.log(prettyPrint(tree.root));
+console.log(tree.isBalanced());
+
+tree.rebalance();
+
+console.log(prettyPrint(tree.root));
+console.log(tree.isBalanced());
---
+++ JS notes/knightsTravails.js
@@ -0,0 +1,113 @@
+class coordinate {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.point = [this.x, this.y];
+    this.previous = null;
+  }
+}
+
+function possibleMove(point) {
+  let move = [
+    [1, 2],
+    [2, 1],
+    [-1, 2],
+    [-2, 1],
+    [1, -2],
+    [2, -1],
+    [-1, -2],
+    [-2, -1],
+  ];
+  let result = [];
+  for (let i = 0; i < move.length; i++) {
+    let newLocation = new coordinate(
+      point.x + move[i][0],
+      point.y + move[i][1]
+    );
+    if (
+      newLocation.x >= 0 &&
+      newLocation.x <= 7 &&
+      newLocation.y >= 0 &&
+      newLocation.y <= 7
+    ) {
+      newLocation.previous = point;
+      result.push(newLocation);
+    }
+  }
+  return result;
+}
+
+function levelOrder(start, end) {
+  const visited = [];
+  const queue = [start];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    visited.push(current);
+    const moves = possibleMove(current);
+    for (let i = 0; i < moves.length; i++) {
+      let isVisited = false;
+      for (let j = 0; j < visited.length; j++) {
+        if (moves[i].x === visited[j].x && moves[i].y === visited[j].y) {
+          isVisited = true;
+          break;
+        }
+      }
+
+      if (!isVisited) {
+        queue.push(moves[i]);
+      }
+    }
+    if (current.x == end.x && current.y == end.y) {
+      return current;
+    }
+  }
+}
+
+function validInput(arr) {
+  if (arr.length !== 2) {
+    return false;
+  }
+  if (arr[0] < 0 || arr[0] > 7 || arr[1] < 0 || arr[1] > 7) {
+    return false;
+  }
+  if (!arr.every(Number.isInteger)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function knightMoves(arr1, arr2) {
+  if (!validInput(arr1) || !validInput(arr2)) {
+    log("Invalid input");
+    return null;
+  }
+  let start = new coordinate(arr1[0], arr1[1]);
+  let end = new coordinate(arr2[0], arr2[1]);
+  let findPath = levelOrder(start, end);
+  let jumps = 0;
+  let result = [];
+  let temporaryNode = findPath;
+  while (temporaryNode.previous) {
+    jumps++;
+    result.push(temporaryNode.point);
+    temporaryNode = temporaryNode.previous;
+  }
+  result.push(start.point);
+  let jumpFromStart = result.reverse();
+  jumpFromStart.push(`${jumps} moves`);
+  log(jumpFromStart);
+}
+
+function log(x) {
+  console.log(x);
+}
+
+//////TESTING//////
+
+// let start = new coordinate(0, 0);
+// let end = new coordinate(7, 7);
+// log(levelOrder(start, end));
+
+knightMoves([0, 0], [1, 0]);
